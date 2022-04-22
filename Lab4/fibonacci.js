81e3863656c48d30038ab74cab0573ae54ef91ad
Yanellys <yanellys.aguilar@utp.ac.pa>
2022-04-22 01:18:19 -0500

    Progress on Lab4

---
+++ Lab4/fibonacci.js
@@ -0,0 +1,21 @@
+
+function printFibonacciNumbers(n) {
+    document.getElementById("amElem").onsubmit();
+    const number = parseInt("amElem");
+    let n1 = 0, n2 = 1, nextTerm;
+    
+    console.log(n1); // print 0
+    console.log(n2); // print 1
+    
+    nextTerm = n1 + n2;
+    
+    while (nextTerm <= number) {
+    
+        // print the next term
+        console.log(nextTerm);
+    
+        n1 = n2;
+        n2 = nextTerm;
+        nextTerm = n1 + n2;
+    }
+}
